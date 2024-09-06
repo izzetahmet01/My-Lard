e4f6a0c3c8e51bd7873691b38ece1849c9abc71f
Aradones <92171325+izzetahmet01@users.noreply.github.com>
2024-09-06 14:27:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,24 +7,23 @@ SCENARIO_PATH = r"C:\Users\STAJYER\Desktop\Codes\RunWay\LARD\scenarios"
 VIDEO_PATH = rf"C:\Users\STAJYER\Desktop\Codes\RunWay\LARD\Videos"
 
 
-# # read_and_create_scenario(DATABASE_PATH,  ICAO_PATH,alpha_deg= 0 , file_name= "")
+#read_and_create_scenario(DATABASE_PATH,  ICAO_PATH,alpha_deg= 0 , file_name= "")
 
-# for dataset_name in os.listdir(SCENARIO_PATH):
-#     print("Processing : ",dataset_name)
-#     csv_file = os.path.join(SCENARIO_PATH,dataset_name,dataset_name,"exported_labels.csv")
-#     exported_file = os.path.join(SCENARIO_PATH,dataset_name,dataset_name,"exported_images")
-#     bbox_folder =  os.path.join(SCENARIO_PATH,dataset_name,dataset_name,"bbox_images")  
+for dataset_name in os.listdir(SCENARIO_PATH):
+    print("Processing : ",dataset_name)
+    csv_file = os.path.join(SCENARIO_PATH,dataset_name,dataset_name,"exported_labels.csv")
+    exported_file = os.path.join(SCENARIO_PATH,dataset_name,dataset_name,"exported_images")
+    bbox_folder =  os.path.join(SCENARIO_PATH,dataset_name,dataset_name,"bbox_images")  
 
-
-#     print("Moves yaml to correct directory")
+    # Moves yaml file to correct directory
+    print("Moves yaml to correct directory")
+    shutil.move(os.path.join(SCENARIO_PATH,dataset_name,str(dataset_name + ".yaml")),os.path.join(SCENARIO_PATH,dataset_name,dataset_name))
     
-#     shutil.move(os.path.join(SCENARIO_PATH,dataset_name,str(dataset_name + ".yaml")),os.path.join(SCENARIO_PATH,dataset_name,dataset_name))
-    
-#     print("Converts labels")
-#     crete_labels(os.path.join(SCENARIO_PATH,dataset_name,dataset_name,str(dataset_name + ".yaml")))
+    # Creates csv file
+    print("Converts labels")
+    crete_labels(os.path.join(SCENARIO_PATH,dataset_name,dataset_name,str(dataset_name + ".yaml")))
 
-#     print("Draws boundary points to images")
-#     draw_boundary_boxes(csv_file,bbox_folder,exported_file)
-#     create_video_from_images(bbox_folder,VIDEO_PATH, fps = 3) 
-
-
+    # Draws boundary boxes and creates images. It is not necessary
+    # print("Draws boundary points to images")
+    # draw_boundary_boxes(csv_file,bbox_folder,exported_file)
+    # create_video_from_images(bbox_folder,VIDEO_PATH, fps = 3) 
