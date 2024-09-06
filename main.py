1bb6466b043ed74025acabace1fda709778b1626
Aradones <92171325+izzetahmet01@users.noreply.github.com>
2024-09-06 14:24:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -25,22 +25,6 @@ VIDEO_PATH = rf"C:\Users\STAJYER\Desktop\Codes\RunWay\LARD\Videos"
 
 #     print("Draws boundary points to images")
 #     draw_boundary_boxes(csv_file,bbox_folder,exported_file)
+#     create_video_from_images(bbox_folder,VIDEO_PATH, fps = 3) 
 
 
-degree = 10
-ICAO_CODE = "UBBB"
-
-# for alpha in range(0,degree + 1,10):
-#     print(alpha)
-#     gerenate_scenario_from_params(DATABASE_PATH,ICAO_CODE,alpha,f"{ICAO_CODE}{alpha}")
-
-# for alpha in range(180 - degree,180,10):
-#     print(alpha)
-#     gerenate_scenario_from_params(DATABASE_PATH,ICAO_CODE,alpha,f"{ICAO_CODE}{alpha}")
-
-# for alpha in range(180 ,180+ degree + 1,10):
-#     print(alpha)
-#     gerenate_scenario_from_params(DATABASE_PATH,ICAO_CODE,alpha,f"{ICAO_CODE}{alpha}")
-gerenate_scenario_from_params(DATABASE_PATH,ICAO_CODE,0,f"{ICAO_CODE}{10}")
-
-gather_files(r"C:\Users\STAJYER\Desktop\Codes\RunWay\LARD\scenarios")
